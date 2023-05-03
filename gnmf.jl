30ee5c4f6cf256df34a9b6af376daa081b78a5f4
Nicholas Richardson <njericha@uwaterloo.ca>
2023-05-03 14:55:30 -0400

    make gnmf

---
+++ gnmf.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.19.22
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 76472511-92f0-471f-89d9-83dbe39003b5
+
+
+# ╔═╡ a3a2a760-e9e2-11ed-26f0-4dfbec8af180
+
+
+# ╔═╡ Cell order:
+# ╠═76472511-92f0-471f-89d9-83dbe39003b5
+# ╠═a3a2a760-e9e2-11ed-26f0-4dfbec8af180
